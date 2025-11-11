3ab6d519496f55f362d864f3c203648b82bda38c
Adrian Rivera Luna <adrianluna0830@gmail.com>
2025-11-10 18:55:08 -0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:vitalingu/german_word.dart';
+import 'package:vitalingu/word.dart';
+
+void main() {
+  runApp(const MaterialApp(home: MyApp()));
+}
