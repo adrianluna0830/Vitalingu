50a7fb65446269caa3ede0a952372737cb90366a
Adrian Rivera Luna <adrianluna0830@gmail.com>
2025-11-11 20:48:12 -0600

    feat: refactor AppStartupLoadingViewModel to improve API key handling and streamline navigation logic

--- lib/viewmodels/app_startup_loading_view_model.dart
+++ lib/viewmodels/app_startup_loading_view_model.dart
@@ -1,18 +1,27 @@
 import 'package:injectable/injectable.dart';
 import 'package:vitalingu/database/app_settings_database.dart';
+import 'package:vitalingu/injection.dart';
+import 'package:vitalingu/services/gemini_prompt_service.dart';
+import 'package:vitalingu/services/pixabay_service.dart';
 import 'package:vitalingu/viewmodels/view_model_base.dart';
+
 @injectable
 class AppStartupLoadingViewModel extends ViewModelBase {
   final AppSettingsDatabase appSettingsDatabase;
-  AppStartupLoadingViewModel({required this.appSettingsDatabase, required super.navigationService});
+  AppStartupLoadingViewModel(
+      {required this.appSettingsDatabase, required super.navigationService});
 
   Future<void> initializeApp() async {
     final String geminiApiKey = await appSettingsDatabase.getGeminiApiKey();
     final String pixabayApiKey = await appSettingsDatabase.getPixabayApiKey();
     if (geminiApiKey.isEmpty || pixabayApiKey.isEmpty) {
       navigationService.goToSettings();
-    } else {
-      navigationService.goToSelectLanguageView();
+      return;
     }
+    getIt.registerSingleton<GeminiPromptService>(
+        GeminiPromptService(geminiApiKey));
+    getIt.registerSingleton<PixabayService>(PixabayService(pixabayApiKey));
+
+    navigationService.goToSelectLanguageView();
   }
 }
