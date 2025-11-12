bd4f9fb3b382344177b35b7224badb3168bd475e
Adrian Rivera Luna <adrianluna0830@gmail.com>
2025-11-11 22:39:00 -0600

    feat: implement WordPromptsService for generating word prompts and handling language translations

---
+++ lib/services/word_prompts_service.dart
@@ -0,0 +1,46 @@
+import 'dart:convert';
+
+import 'package:vitalingu/language/language.dart';
+import 'package:vitalingu/services/gemini_prompt_service.dart';
+import 'package:vitalingu/word/word.dart';
+
+class WordPromptsService {
+  final Language nativeLanguage;
+  final Language targetLanguage;
+  final GeminiPromptService geminiPromptService;
+
+  WordPromptsService({required this.nativeLanguage, required this.targetLanguage, required this.geminiPromptService});
+  Future<Word?> _getWordInDatabase(String word) async
+  {
+    return null;
+  }
+
+  Future<String> _getWordLema(String word) async
+  {
+        String prompt = "Return the word lema for the word '$word' in ${targetLanguage.bcp47Code} language.";
+        return await geminiPromptService.generatePrompt(prompt);
+  }
+
+  String _getWordPrompt(String wordLema)
+  {
+     return targetLanguage.languageWord.getWordPrompt(word: wordLema, nativeLanguage: nativeLanguage.bcp47Code, targetLanguage: targetLanguage.bcp47Code);
+  }
+
+  Future<Word> getWord(String word) async
+  {
+    Word? wordInDatabase = await _getWordInDatabase(word);
+    if (wordInDatabase != null) {
+      return wordInDatabase;
+    }
+
+    String wordLema = await _getWordLema(word);
+    String wordPrompt = _getWordPrompt( wordLema);
+
+    String geminiPrompt = await geminiPromptService.generatePrompt(wordPrompt);
+    return targetLanguage.languageWord.fromJson(geminiPrompt);
+  }
+
+
+
+
+}
