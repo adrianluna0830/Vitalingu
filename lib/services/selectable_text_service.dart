e22e7f147748a6c3a7caeff9c592fd19b13ce658
Adrian Rivera Luna <adrianluna0830@gmail.com>
2025-11-17 00:20:36 -0600

    refactor: improve selectable text prompts

--- lib/services/selectable_text_service.dart
+++ lib/services/selectable_text_service.dart
@@ -12,11 +12,12 @@ class SelectableTextService {
   Future<String> getDoubtResponse(String doubt,String selection, String selectionEnbracketedInFullText) async {
     String prompt = """
 Answer the following question related to the given text.
+You cannot use sybols like * or - in your answer. Only comas, periods, "" marks, and question marks are allowed.
 Text where my Doubt is: "$selection"
 Full context: "$selectionEnbracketedInFullText"
 The question related to the text:
 Question: "$doubt"
-Provide the answer in ${_nativeLanguage.language!.nativeLanguageName}.
+Provide a direct and conscice answer (if possible) in ${_nativeLanguage.language!.nativeLanguageName}, except is user request to use the target language.
 """;
     final response = await geminiPromptService.generatePrompt(prompt);
     return response;
@@ -27,7 +28,7 @@ Provide the answer in ${_nativeLanguage.language!.nativeLanguageName}.
 Translate the following text from ${_targetLanguage.language!.nativeLanguageName} to ${_nativeLanguage.language!.nativeLanguageName}:
 Specific text: "$selection"
 Full context: "$selectionEnbracketedInFullText"
-Provide the answer in ${_nativeLanguage.language!.nativeLanguageName}.
+Provide a direct and conscice answer.
 
 """;
     final response = await geminiPromptService.generatePrompt(prompt);
