2081a4f3c87c550b66692685fa43336073ea6459
Adrian Rivera Luna <adrianluna0830@gmail.com>
2026-01-16 17:24:59 -0600

    Update SupportedLanguagesBcp47 enum: change language codes to BCP47 format and enhance native name retrieval

--- lib/models/language/supported_languages_bcp47_enum.dart
+++ lib/models/language/supported_languages_bcp47_enum.dart
@@ -1,14 +1,23 @@
 enum SupportedLanguagesBcp47 {
-  en,
-  es,
+  en_US,
+  es_MX,
 }
 
 extension SupportedLanguagesExtension on SupportedLanguagesBcp47 {
+
+  String get completeBCP47 {
+    switch (this) {
+      case SupportedLanguagesBcp47.en_US:
+        return "en-US";
+      case SupportedLanguagesBcp47.es_MX:
+        return "es-MX";
+    }
+  }
   String get nativeName {
     switch (this) {
-      case SupportedLanguagesBcp47.en:
+      case SupportedLanguagesBcp47.en_US:
         return "English";
-      case SupportedLanguagesBcp47.es:
+      case SupportedLanguagesBcp47.es_MX:
         return "Español";
     }
   }
