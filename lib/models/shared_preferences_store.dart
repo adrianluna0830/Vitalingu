dd9fc6def3de0c21f35128f5b85b969e62f21be2
Adrian Rivera Luna <adrianluna0830@gmail.com>
2026-01-20 23:46:48 -0600

    Feature: Implemented UserAppSettings to manage global settings using signals. Added flutter_secure_storage for securely storing sensitive data like API keys.

--- lib/models/shared_preferences_store.dart
+++ lib/models/shared_preferences_store.dart
@@ -1,6 +1,8 @@
 import 'package:injectable/injectable.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:signals/signals.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
 
 @injectable
 class SharedPreferencesStore implements SignalsKeyValueStore {
@@ -29,6 +31,32 @@ class SharedPreferencesStore implements SignalsKeyValueStore {
   Future<void> setItem(String key, String value) async {
     final prefs = await init();
     prefs.setString(key, value);
+  }
+}
+
+
+@injectable
+class EncryptedSharedPreferencesStore implements SignalsKeyValueStore {
+  EncryptedSharedPreferencesStore();
+
+  final FlutterSecureStorage _storage = const FlutterSecureStorage();
+
+  @override
+  Future<String?> getItem(String key) async {
+    return await _storage.read(key: key);
+  }
+
+  @override
+  Future<void> setItem(String key, String value) async {
+    await _storage.write(
+      key: key,
+      value: value,
+    );
+  }
+
+  @override
+  Future<void> removeItem(String key) async {
+    await _storage.delete(key: key);
   }
 }
 
---
+++ lib/models/user_app_settings.dart
@@ -0,0 +1,92 @@
+
+import 'package:injectable/injectable.dart';
+import 'package:signals/signals.dart';
+import 'package:vitalingu/models/language/language_enum.dart';
+import 'package:vitalingu/models/shared_preferences_store.dart';
+@singleton
+class NativeLanguageSignal extends PersistedEnumSignal<Language> {
+  NativeLanguageSignal({required SharedPreferencesStore sharedPreferencesStore})
+      : super(
+          Language.English,
+          'nativeLanguage', 
+          Language.values, 
+          store: sharedPreferencesStore,
+        );
+
+  @FactoryMethod(preResolve: true)
+  static Future<NativeLanguageSignal> create({
+    required SharedPreferencesStore sharedPreferencesStore
+  }) async {
+    final signal = NativeLanguageSignal(
+      sharedPreferencesStore: sharedPreferencesStore
+    );
+    await signal.init();
+    return signal;
+  }
+}
+
+@singleton
+class TargetLanguageSignal extends PersistedEnumSignal<Language> {
+  TargetLanguageSignal({required SharedPreferencesStore sharedPreferencesStore})
+      : super(
+          Language.Spanish,
+          'targetLanguage',  
+          Language.values,
+          store: sharedPreferencesStore,
+        );
+
+  @FactoryMethod(preResolve: true)
+  static Future<TargetLanguageSignal> create({
+    required SharedPreferencesStore sharedPreferencesStore
+  }) async {
+    final signal = TargetLanguageSignal(
+      sharedPreferencesStore: sharedPreferencesStore
+    );
+    await signal.init();
+    return signal;
+  }
+}
+@singleton
+class GeminiApiKeySignal extends PersistedSignal<String> {
+  GeminiApiKeySignal({
+    required EncryptedSharedPreferencesStore encryptedStore,
+  }) : super(
+          '',
+          key: 'gemini_api_key',
+          store: encryptedStore,
+          autoInit: false,
+        );
+
+  @FactoryMethod(preResolve: true)
+  static Future<GeminiApiKeySignal> create({
+    required EncryptedSharedPreferencesStore encryptedStore,
+  }) async {
+    final signal =
+        GeminiApiKeySignal(encryptedStore: encryptedStore);
+    // await signal.init();
+    return signal;
+  }
+}
+
+
+@singleton
+class StopTrackingAtRatioSignal extends PersistedSignal<double> {
+  StopTrackingAtRatioSignal({
+    required SharedPreferencesStore sharedPreferencesStore,
+  }) : super(
+          0.8,
+          key: 'stop_tracking_at_value',
+          store: sharedPreferencesStore,
+          autoInit: false,
+        );
+
+  @FactoryMethod(preResolve: true)
+  static Future<StopTrackingAtRatioSignal> create({
+    required SharedPreferencesStore sharedPreferencesStore,
+  }) async {
+    final signal =
+        StopTrackingAtRatioSignal(sharedPreferencesStore: sharedPreferencesStore);
+    await signal.init();
+    return signal;
+  }
+}
